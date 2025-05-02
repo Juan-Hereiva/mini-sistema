0787d2ecfc12cd72a7129a3f2371219b1ddb17a8
Juan-Hereiva <111255173+Juan-Hereiva@users.noreply.github.com>
2025-05-02 17:10:44 -0600

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,14 @@
+# tarea.py
+
+class Tarea:
+    def __init__(self, nombre, descripcion):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.completada = False
+
+    def marcar_completada(self):
+        self.completada = True
+
+    def __str__(self):
+        estado = "✅ Completada" if self.completada else "❌ Pendiente"
+        return f"Tarea: {self.nombre}\nDescripción: {self.descripcion}\nEstado: {estado}"
